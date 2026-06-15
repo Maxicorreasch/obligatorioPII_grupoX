6e1c8872e461054a5ceeef0344e13d9ee9a1b36f
maxicorreasch <maxicorreasch@gmail.com>
2026-06-14 21:53:54 -0300

    Metodos agregados al AdministradorProcesos:
    - ejecutarProcesos
    - finalizarProcesos
    - pload (incompleto), falta agregar el cargarPRocesos

--- src/main/java/uy/edu/um/doors/AdministradorProcesos.java
+++ src/main/java/uy/edu/um/doors/AdministradorProcesos.java
@@ -1,13 +1,21 @@
 package uy.edu.um.doors;
 
 import lombok.Getter;
+import uy.edu.um.tad.hash.MyHash;
+import uy.edu.um.tad.hash.MyHashImpl;
+import uy.edu.um.tad.heap.EmptyHeapException;
 import uy.edu.um.tad.heap.MyHeap;
 import uy.edu.um.tad.heap.MyHeapImpl;
 import uy.edu.um.tad.queue.EmptyQueueException;
 import uy.edu.um.tad.queue.MyQueue;
 import uy.edu.um.tad.queue.MyQueueImpl;
+import uy.edu.um.tad.stack.EmptyStackException;
 import uy.edu.um.tad.stack.MyStack;
 import uy.edu.um.tad.stack.MyStackImpl;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 @Getter
@@ -24,24 +32,99 @@ public class AdministradorProcesos {
 
     private MyStack<Proceso> procesosTerminados;
 
+    private MyHash<Integer, Usuario> usuarios;
+
 
 
     public AdministradorProcesos() {
         procesosNew = new MyQueueImpl<>();
         procesosPending = new MyHeapImpl<>(false);
         procesosTerminados = new MyStackImpl<>();
+        usuarios = new MyHashImpl<>();
         procesosRunning = null;
     }
 
-    public void pepararProcesos() throws EmptyQueueException {
+    public void prepararProcesos() throws EmptyQueueException {
 
-        while(!procesosTerminados.isEmpty()){
+        while(!procesosNew.isEmpty()){
             Proceso proceso = procesosNew.dequeue();
 
             proceso.calcularPrioridad();
             proceso.setEstado("PENDING");
             procesosPending.insert(proceso);
         }
+    }
+
+    public void ejecutarProcesos() throws EmptyHeapException {
+        if (procesosRunning == null) {
+            Proceso proceso = procesosPending.remove();
+
+            proceso.setEstado("RUNNING");
+
+            procesosRunning = proceso;
+        }
+    }
+
+    public void finalizarProceso()throws EmptyStackException {
+
+
+
+        if (procesosRunning == null) {
+            return;
+        }
+
+        if(procesosTerminados.size() == MAX_PROCESOS_TERMINADOS){
+            while(!procesosTerminados.isEmpty()){
+                Proceso proceso = procesosTerminados.pop();
+                System.out.println(proceso.toString());
+            }
+        }
+
+
+        procesosRunning.setEstado("FINISHED");
+        procesosTerminados.push(procesosRunning);
+
+        procesosRunning = null;
+
+    }
+
+    public void agregarUsuario(Usuario usuario) {
+        usuarios.put(usuario.getUID(), usuario);
+    }
+
+    private void cargarUsuarios(String pathUsuarios){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(pathUsuarios));
+
+            String linea;
+
+            br.readLine();
+
+            while ((linea = br.readLine()) != null){
+                String[] datos = linea.split(";");
+
+                int uid = Integer.parseInt(datos[0]);
+                String alias = datos[1];
+                String tipo = datos[2];
+
+                Usuario usuario = new Usuario(uid, alias, tipo);
+
+                agregarUsuario(usuario);
+
+            }
+            br.close();
+        } catch (IOException e){
+            System.out.println("Error al cargar el usuario");
+        }
+
+    }
+
+    public void pload(String pathProcesos, String pathUsuarios){
+
+        cargarUsuarios(pathUsuarios);
+
+        cargarProcesos(pathProcesos);
+
     }
 
 
